f269bbc1da2e4d7378ec59ad04fa12fb9385511e
tsunemori-akane <po6271933@outlook.com>
2020-03-08 20:10:49 +0800

    组个最小数

---
+++ 乙级/1023.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int n, t = 0, r[10];
+	string s1, s2;
+	for (int i = 0; i < 10; i++) {
+		cin >> n;
+		r[i] = n;
+		t += n;
+	}
+	for (int i = 1; i < 10; i++) {
+		if (r[0] == 0) {
+			for (int j = 0; j < r[i]; j++) {
+				cout << i;
+			}
+		}
+		else {
+			for (int j = 0; j < r[i]; j++) {
+				s1 = s1 + to_string(i);
+			}
+		}
+	}
+	if (r[0] != 0) {
+		while (r[0] > 0) {
+			s2 = s2 + '0';
+			r[0] --;
+		}
+		cout << s1[0] + s2 + s1.substr(1, t-r[0]-1);
+	}
+	return 0;
+}
