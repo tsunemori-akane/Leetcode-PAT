9f0c7ee3c4da6873c9ae2ad0f5e0419f0c5cf2dd
tsunemori-akane <39367561+tsunemori-akane@users.noreply.github.com>
2021-12-28 17:39:23 +0800

    双指针

--- leetcode/0633/0633.cpp
+++ leetcode/0633/0633.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        unsigned int l = 0, r = (unsigned int) sqrt(c);
+        while(l <= r) {
+            unsigned int res = l*l + r*r;
+            if(res < c) {
+                l++;
+            }
+            else if(res > c) {
+                r--;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
