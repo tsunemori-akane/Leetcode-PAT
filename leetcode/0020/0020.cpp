59f2b9540f7058acd2e7adeee9df49fc5c5835a8
tsunemori-akane <39367561+tsunemori-akane@users.noreply.github.com>
2022-03-14 22:57:45 +0800

    栈

--- leetcode/0020/0020.cpp
+++ leetcode/0020/0020.cpp
@@ -5,29 +5,27 @@
 using namespace std;
 
 class Solution {
-  public: 
-  bool isValid(string s) {
-    if(s[0] == '}' || s[0] == ')' || s[0] == ']') {
-      return false;
-    }
-    
-    stack<char> x;
-    x.push(s[0]);
-    for(int i = 1; i < s.length(); ++i) {
-      if(!x.empty()) {
-        string str;
-        if( str + x.top() + s[i] == "{}" || str + x.top() + s[i] == "[]" || str + x.top() + s[i] == "()") {
-          x.pop();
-          cout << x.top() << endl;
-        } else {
-          x.push(s[i]);
+public:
+    bool isValid(string s) {
+        if(s[0] == '}' || s[0] == ')' || s[0] == ']') {
+            return false;
         }
-      }
+        stack<char> x;
+        for(int i = 0; i < s.length(); ++i) {
+            if(!x.top()) {
+                string str;
+                if( str + x.top() + s[i] == "{}" || str + x.top() + s[i] == "[]" || str + x.top() + s[i] == "()") {
+                    x.pop();
+                }
+            } else {
+                x.push(s[i]);
+            }
+        }
+        if(x.empty()) return true ;
+        else return false;
     }
-    if(x.empty()) return true ;
-    else return false;
-  }
 };
+
 
 void printBool(bool res){
     cout << (res ? "True" : "False") << endl;
