6f35ab9390edd4723c378c72b249d39966f865e9
tsunemori-akane <39367561+tsunemori-akane@users.noreply.github.com>
2022-03-23 14:52:24 +0800

    单向链表

--- leetcode/0328/0328.cpp
+++ leetcode/0328/0328.cpp
@@ -1,28 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (!head) return head;
-        ListNode* evenHead = head->next;        //奇头是head，偶头就是head->next
-        ListNode* odd = head, *even = evenHead; //分别定义奇偶尾指针
-
-        while (even && even->next)
-        {
-            odd = odd->next = even->next;       //等于 odd->next = even->next; odd = odd->next;
-            even = even->next = odd->next;
-        }
-
-        odd->next = evenHead;
-
-        return head;
+      if(head == NULL || head->next == NULL || head->next->next == NULL)
+            return head;
+  
+      ListNode* p1 = head;
+      ListNode* p2 = head->next;
+      ListNode* p3 = p2;
+      while(p1 != NULL && p1->next != NULL && p2->next != NULL) {
+        p1->next = p1->next->next;
+        p1 = p1->next;
+        p2->next = p2->next->next;
+        p2 = p2->next;
+      }
+      p1->next = p3;
+      return head;
     }
 };
